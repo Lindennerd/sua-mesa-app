6db2b995cf5f116f32442ada3a00af24a55c54c5
Biblioteca Revolucionaria <xicamauro@protonmail.ch>
2022-12-04 17:59:06 -0300

    feat: building the landing page

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,21 +1,47 @@
-import { useAuth } from '@redwoodjs/auth';
-import { navigate, routes } from '@redwoodjs/router';
-import { MetaTags } from '@redwoodjs/web';
+import { Container, createStyles, Flex, Group, Text } from '@mantine/core'
 
+import { useAuth } from '@redwoodjs/auth'
 
 const HomePage = () => {
-
-  const { logOut, currentUser } = useAuth();
+  const { logOut, currentUser } = useAuth()
+  const { classes } = useStyles()
 
   return (
     <>
-      <MetaTags title="Home" description="Home page" />
-      Landing Page
-      <p>{ JSON.stringify(currentUser) }</p>
-      <button onClick={(e) => navigate(routes.login())}>Sign In</button>
-      <button onClick={(e) => logOut()}>Sign Out</button>
+      <div className={classes.hero}>
+        <Container>
+          <Group>
+            <img src="sua-mesa-logo.png" alt="logo" height="150" />
+          </Group>
+        </Container>
+
+        <Flex className={classes.heroSection}>
+          <Text className={classes.heroTitle}>SUA MESA</Text>
+          <Text size="xl" align="center">
+            O jeito mais fácil de trazer agilidade no atendimento dos seus
+            clientes
+          </Text>
+        </Flex>
+      </div>
     </>
   )
 }
+
+const useStyles = createStyles((theme) => ({
+  hero: {
+    backgroundColor: theme.colors.red[7],
+    color: 'white',
+  },
+  heroTitle: {
+    fontSize: '5em',
+    textAlign: 'center',
+  },
+  heroSection: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    justifyItems: 'center',
+    padding: 'lg',
+  },
+}))
 
 export default HomePage
